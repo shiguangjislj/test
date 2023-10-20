c963e31cc702d0f0cf60f3ebd476d1e4fbdd6bb6
shiguangji <3336060061@qq.com>
2023-10-20 22:22:27 +0800

    add u2 u3

---
+++ U2/10_19.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("try");
+
+return 0;
+}
---
+++ U2/2_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a=4;
+    b=5;
+    double r;
+   r=(double)a/b;
+    printf(":%5.1f",r);
+   // printf("aÊÇbµÄ%f%%",(double) a/b*100);
+   return 0;
+}
